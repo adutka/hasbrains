6f394343fdfeef23ac575540641a7ec61472ce66
adutka <adutka@ukr.net>
2015-07-06 18:13:48 +0300

    task023 private and public

---
+++ 023/init.rb
@@ -0,0 +1,16 @@
+require_relative "cart"
+require_relative "item"
+require_relative "virtual_item"
+require_relative "real_item"
+
+item1 = VirtualItem.new({ :price => 10, :weight => 100, :name => "Car"})
+item2 = RealItem.new({ :weight => 100, :name => "Car"})
+
+cart = Cart.new
+cart.add_item item1
+cart.add_item item2
+
+puts item1.price
+
+
+puts item1.real_price
