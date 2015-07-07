bd16f027260e802cba86090266617b62a2ebc8cf
adutka <adutka@ukr.net>
2015-07-07 23:51:00 +0300

    25 task submodules

---
+++ 025/init.rb
@@ -0,0 +1,26 @@
+require_relative "item_container"
+require_relative "cart"
+require_relative "order"
+require_relative "item"
+require_relative "virtual_item"
+require_relative "real_item"
+
+item1 = VirtualItem.new({ :price => 10, :weight => 100, :name => "Car"})
+item2 = RealItem.new({ :price => 20,:weight => 100, :name => "Car"})
+item3 = RealItem.new({ :price => 30,:weight => 50, :name => "BlaBlaCar"})
+
+cart = Cart.new
+cart.add_item item1
+cart.add_item item2
+cart.remove_item
+puts cart.items.size
+
+
+order = Order.new
+order.add_item item1
+order.add_item item2
+order.add_item item3
+order.remove_item
+puts order.items.size
+
+puts order.count_valid_items
---
+++ 025/order.rb
@@ -0,0 +1,15 @@
+class Order
+
+  attr_reader :items
+
+  include ItemContainer::Manager
+  include ItemContainer::Info
+
+  def initialize
+    @items = Array.new
+  end
+
+  def place
+    #actually place an order
+  end
+end
