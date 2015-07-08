72be1ffcadc35ef888c2eb9fac1e1b281f8867a2
adutka <adutka@ukr.net>
2015-07-08 22:13:05 +0300

    input from command line

---
+++ 027/init.rb
@@ -0,0 +1,11 @@
+require_relative "item_container"
+require_relative "cart"
+require_relative "order"
+require_relative "item"
+require_relative "virtual_item"
+require_relative "real_item"
+
+@items = []
+@items << VirtualItem.new({ :price => 10, :weight => 100, :name => "car"})
+@items << RealItem.new({ :price => 121,:weight => 100, :name => "card"})
+@items << RealItem.new({ :price => 130,:weight => 50, :name => "blablacar"})
